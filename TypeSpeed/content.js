c63dfe9a1f86ec66630a4bb6a0247d740d8cb6eb
Abraham McIlvaine <abemac@udel.edu>
2015-12-29 21:48:36 -0500

    new stuff abraham

--- TypeSpeed/content.js
+++ TypeSpeed/content.js
@@ -1,5 +1,11 @@
 window.onkeydown=function(e){
+    if(e.keyCode==32){
+      chrome.runtime.sendMessage({greeting:"spacePressed"},function(response){
+        console.log(response.farewell);
+      });
+    }else{
     chrome.runtime.sendMessage({greeting:"keyPressed"},function(response){
       console.log(response.farewell);
     });
+  }
 }
