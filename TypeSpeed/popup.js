dbbf311530e06eab9ce698ef4f307aa91bf74f7d
Abraham McIlvaine <abemac@udel.edu>
2015-12-29 22:28:08 -0500

    new stuff abraham

--- TypeSpeed/popup.js
+++ TypeSpeed/popup.js
@@ -2,3 +2,10 @@ function doStuff() {
   document.getElementById("numkeys").innerHTML=chrome.extension.getBackgroundPage().wpm;
 }
 document.addEventListener('DOMContentLoaded',function(){doStuff();});
+
+function reset(){
+	chrome.extension.getBackgroundPage().wpm=0;
+	chrome.extension.getBackgroundPage().spaceCount=0;
+	chrome.extension.getBackgroundPage().wordTyped=false;
+	chrome.extension.getBackgroundPage().elapsedTime=0;
+}
